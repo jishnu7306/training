633658acd3b0eddeb2b394a621de8cc0fa0fad3a
jishnu <jishnuksatheeshsj143@gmail.com>
2022-03-18 01:37:37 +0530

    jishnu

--- Training/views.py
+++ Training/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def manager_Dashboard(request):
+    return render(request,'manager\manager_Dashboard.html')
+
+def manager_categories(request):
+    return render(request,'manager\manager_categories.html') 
+
+def manager_emp_categories(request):
+    return render(request,'manager\manager_emp_categories.html')   
+
+def manager_course(request):
+    return render(request,'manager\manager_course.html')
+
+def manager_emp_course_list(request):
+    return render(request,'manager\manager_emp_course_list.html')
+
+def manager_emp_course_details(request):
+    return render(request,'manager\manager_emp_course_details.html')
+
+def manager_emp_profile(request):
+    return render(request,'manager\manager_emp_profile.html')
+
+def manager_emp_involved_projects(request):
+    return render(request,'manager\manager_emp_involved_projects.html')
+    
+def manager_emp_attendance(request):
+    return render(request,'manager\manager_emp_attendance.html')
+
+def manager_emp_attendance_show(request):
+    return render(request,'manager\manager_emp_attendance_show.html')
+
+def manager_task(request):
+    return render(request,'manager\manager_task.html')
+
+def manager_givetask(request):
+    return render(request,'manager\manager_givetask.html')
+
+def manager_current_task(request):
+    return render(request,'manager\manager_current_task.html')
+
+def manager_previous_task(request):
+    return render(request,'manager\manager_previous_task.html')
+
+def manager_registration_details(request):
+    return render(request,'manager\manager_registration_details.html')  
+
+def manager_attendance(request):
+    return render(request,'manager\manager_attendance.html') 
+
+def manager_attendance_show(request):
+    return render(request,'manager\manager_attendance_show.html')
+
+def manager_reported_issues(request):
+    return render(request,'manager\manager_reported_issues.html') 
+
+def manager_issues(request):
+    return render(request,'manager\manager_issues.html')
+
+def manager_issues_form(request):
+    return render(request,'manager\manager_issues_form.html')
+
+def manager_myissues(request):
+    return render(request,'manager\manager_myissues.html')
+
+def manager_reported_detail(request):
+    return render(request,'manager\manager_reported_detail.html')
+
+def manager_reported_issue_show(request):
+    return render(request,'manager\manager_reported_issue_show.html')
--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,32 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from Training import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+
+    path('manager_Dashboard/', views.manager_Dashboard, name='manager_Dashboard'),
+    path('manager_categories/', views.manager_categories, name='manager_categories'),
+    path('manager_emp_categories/', views.manager_emp_categories, name='manager_emp_categories'),
+    path('manager_course/', views.manager_course, name='manager_course'),
+    path('manager_emp_course_list/', views.manager_emp_course_list,name='manager_emp_course_list'),
+    path('manager_emp_course_details/', views.manager_emp_course_details,name='manager_emp_course_details'),
+    path('manager_emp_profile/', views.manager_emp_profile,name='manager_emp_profile'),
+    path('manager_emp_attendance/', views.manager_emp_attendance,name='manager_emp_attendance'),
+    path('manager_emp_attendance_show/', views.manager_emp_attendance_show,name='manager_emp_attendance_show'),
+    path('manager_task/', views.manager_task, name='manager_task'),
+    path('manager_givetask/', views.manager_givetask, name='manager_givetask'),
+    path('manager_current_task/', views.manager_current_task,name='manager_current_task'),
+    path('manager_previous_task/', views.manager_previous_task,name='manager_previous_task'),
+    path('manager_registration_details/', views.manager_registration_details,name='manager_registration_details'),
+    path('manager_attendance/', views.manager_attendance,name='manager_attendance'),
+    path('manager_attendance_show/', views.manager_attendance_show,name='manager_attendance_show'),
+    path('manager_reported_issues/', views.manager_reported_issues,name='manager_reported_issues'),
+    path('manager_issues/', views.manager_issues, name='manager_issues'),
+    path('manager_issues_form/', views.manager_issues_form,name='manager_issues_form'),
+    path('manager_myissues/', views.manager_myissues, name='manager_myissues'),
+    path('manager_reported_detail/', views.manager_reported_detail,name='manager_reported_detail'),
+    path('manager_reported_issue_show/', views.manager_reported_issue_show,name='manager_reported_issue_show'),
 ]
